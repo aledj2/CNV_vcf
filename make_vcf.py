d9ba58210157bf1633d61c3b579d5578ddcf9c00
Aled Jones <aledjones@nhs.net>
2016-09-12 16:43:31 +0100

    cycle through files in folder. used to make first 6 vcfs

--- make_vcf.py
+++ make_vcf.py
@@ -4,18 +4,21 @@ This script reads an aberration report and performs a blat search to get the ref
 @author: aled
 '''
 import subprocess
-
+import os
 
 class create_vcf():
 
     def __init__(self):
         self.base_template = "/home/aled/Documents/CNV_vcf/cnv_template.vcf"
-        self.aber_report = "/home/aled/Documents/CNV_vcf/aber_report.txt"
-        self.output_vcf = "/home/aled/Documents/CNV_vcf/output/output_vcf.vcf"
+        self.aber_report_folder = "/home/aled/Documents/CNV_vcf/aber_reports/"
+        self.output_vcf = "/home/aled/Documents/CNV_vcf/output/output_"
+        self.aber_report=""
+        self.outputfilename=""
 
-    def read_aber_report(self):
+    def read_aber_report(self, filein):
+        self.outputfilename=self.output_vcf+filein.replace('csv','vcf')
         # open the output file
-        output_file = open(self.output_vcf, 'w')
+        output_file = open(self.outputfilename, 'w')
 
         # open base template and copy it to output file
         vcf_header = open(self.base_template, 'r')
@@ -26,12 +29,13 @@ class create_vcf():
         vcf_header.close()
         output_file.close()
 
+        self.aber_report=self.aber_report_folder+filein
         # send each line of aber report to get_first_base
         aber_report = open(self.aber_report, 'r')
 
         # loop through the aber_report and pass line to get_first_base module
         for line in aber_report:
-            create_vcf().get_first_base(line)
+            self.get_first_base(line)
         aber_report.close()
 
     def get_first_base(self, line):
@@ -57,7 +61,7 @@ class create_vcf():
         ref_base = out[-2:-1]
 
         # pass line and ref base to module to write vcf
-        create_vcf().write_to_output_file(ref_base, line)
+        self.write_to_output_file(ref_base, line)
 
     def write_to_output_file(self, ref_base, line):
         # split line
@@ -70,34 +74,34 @@ class create_vcf():
         del_score=float(linesplit[7])
         # calculate length of CNV
         length = int(end) - int(start)
-        #ignore the sex chroms 
-        if chr in ("chrX","chrY"):
-            pass
+        
+        #set del or dup for the SVTYPE and the copy number based on the score
+        if dup_score > 0.4:
+            ALT="DUP"
+            CN="3"
+        elif del_score < -0.8:
+            ALT="DEL"
+            CN="1"
         else:
-            #set del or dup for the SVTYPE and the copy number based on the score
-            if dup_score > 0.4:
-                ALT="DUP"
-                CN="3"
-            elif del_score < -0.8:
-                ALT="DEL"
-                CN="1"
-            else:
-                ALT="CNV"
-                CN="2"
-                
-            # open output file
-            output_file = open(self.output_vcf, 'a')
-    
-            # an example vcf line:
-            # CHROM    POS    ID    REF    ALT    QUAL    FILTER    INFO    FORMAT    SAMPLE
-            # chr1    1583259    .    REF    <CNV>    100.0    .    PRECISE=FALSE;SVTYPE=CNV;END=1655696;LEN=72437;NUMTILES=70;CONFIDENCE=10;PRECISION=10    GT:GQ:CN    ./.:0:3
-    
-            # write line
-            output_file.write("\n" + chr + "\t" + start + "\t.\t" + ref_base + "\tCNV\t100.0\t.\tPRECISE=FALSE;SVTYPE="+ALT+";END=" + str(end) + ":LEN=" + str(length) + ":NUMTILES=" + str(num_probes) + ":CONFIDENCE=0;PRECISION=45\tGT:GQ:CN\t./.:0:"+CN)
-            # close file
-            output_file.close()
+            ALT="CNV"
+            CN="2"
+        
+        print self.outputfilename
+        # open output file
+        output_file = open(self.outputfilename, 'a')
+
+        # an example vcf line:
+        # CHROM    POS    ID    REF    ALT    QUAL    FILTER    INFO    FORMAT    SAMPLE
+        # chr1    1583259    .    REF    <CNV>    100.0    .    PRECISE=FALSE;SVTYPE=CNV;END=1655696;LEN=72437;NUMTILES=70;CONFIDENCE=10;PRECISION=10    GT:GQ:CN    ./.:0:3
+
+        # write line
+        output_file.write("\n" + chr + "\t" + start + "\t.\t" + ref_base + "\tCNV\t100.0\t.\tPRECISE=FALSE;SVTYPE="+ALT+";END=" + str(end) + ":LEN=" + str(length) + ":NUMTILES=" + str(num_probes) + ":CONFIDENCE=0;PRECISION=45\tGT:GQ:CN\t./.:0:"+CN)
+        # close file
+        output_file.close()
 
 
 if __name__ == "__main__":
-    a = create_vcf()
-    a.read_aber_report()
+    for file in os.listdir('/home/aled/Documents/CNV_vcf/aber_reports'):
+        print file
+        a = create_vcf()
+        a.read_aber_report(file)
